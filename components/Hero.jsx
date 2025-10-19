6992bb48f0c9d9c54f008cf5dfc42aff87e4ddc5
Om Barvekar <jayaschool1981@gmail.com>
2025-10-19 18:45:50 +0530

    Updated Hero section design with particles and resume button

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -51,7 +51,7 @@ export default function Hero() {
         <div className="mt-10 flex gap-4 justify-center md:justify-start">
           <a href="#projects" className="btn-primary">View My Work</a>
           <a href="#contact" className="btn-outline">Hire Me</a>
-          <a href="/ombarvekarCV2.pdf" className="btn-primary" download>Download Resume</a>
+          <a href="/ombarvekarCV3.pdf" className="btn-primary" download>Download Resume</a>
         </div>
       </div>
 
